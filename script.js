e7305e71316e997eaf3771771f6aa3cf0d591eae
coder-noman <abdhullaalnoman25@gmail.com>
2025-09-07 23:19:13 +0600

    third commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//bts ="";
-
 //.........websocket_client code..............
 var socket = new WebSocket('ws://27.147.170.162:81');
 socket.onmessage = function (event) {
@@ -81,46 +79,3 @@ allDropdown.forEach(item => {
 		item.classList.toggle('show');
 	})
 })
-
-// main 
-
-//power supply unit
-
-const bgpPSU1 = document.getElementById('bgp-psu1');
-const bgpPSU2 = document.getElementById('bgp-psu2');
-const fortinetPSU1 = document.getElementById('fortinet-psu1');
-const fortinetPSU2 = document.getElementById('fortinet-psu2');
-const checkPointPSU1 = document.getElementById('check-point-psu1');
-const checkPointPSU2 = document.getElementById('check-point-psu2');
-const ciscoPsu = document.getElementById('cisco-psu');
-const lanPsu = document.getElementById('lan-psu');
-const ciscoDistPsu = document.getElementById('cisco-distribution-psu');
-const hoDrPsu1 = document.getElementById('ho-dr-psu1');
-const hoDrPsu2 = document.getElementById('ho-dr-psu2');
-const hoServicePsu1 = document.getElementById('ho-service-psu1');
-const hoServicePsu2 = document.getElementById('ho-service-psu2');
-const pabxPsu = document.getElementById('pabx-psu');
-const nvrPsu = document.getElementById('nvr-psu');
-const r7301Psu1 = document.getElementById('r730-1-psu1');
-const r7301Psu2 = document.getElementById('r730-1-psu2');
-const r7302Psu1 = document.getElementById('r730-2-psu1');
-const r7302Psu2 = document.getElementById('r730-2-psu2');
-const sanSw1psu1 = document.getElementById('san-sw1-psu1');
-const sanSw1psu2 = document.getElementById('san-sw1-psu2');
-const sanSwpsu1 = document.getElementById('san-sw-psu1');
-const sanSwpsu2 = document.getElementById('san-sw-psu2');
-const sanSoragepsu1 = document.getElementById('san-sorage-psu1');
-const sanSoragepsu2 = document.getElementById('san-sorage-psu2');
-
-// bgpPSU1.innerText = 'OFF'
-
-
-// Others Alarm Unit
-
-const waterLeakage = document.getElementById('water-leakage');
-const fireAlarm = document.getElementById('fire-Alarm');
-const generatorStatus = document.getElementById('generator-status');
-const ups1CB = document.getElementById('ups1-cb-status');
-const ups2CB = document.getElementById('ups2-cb-status');
-
-ups2CB.innerText = 'OK';
